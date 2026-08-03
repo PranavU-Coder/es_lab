1f8a3e5efacbd22332c52586201322fa80fa900c
pranav <pranavu8406@gmail.com>
2026-08-03 15:40:36 +0530

    chore: program 4

---
+++ lab_0/pre_post.s
@@ -0,0 +1,26 @@
+	AREA PROGRAM1, CODE, READONLY
+ENTRY
+	ADR R0, Q
+	LDR R1, [R0]
+	ldrb r2, [r0]
+	ldrb r3, [r0]
+	LDR R4, =RES
+	STR R1, [r4]
+	strb r2, [r4, #4]
+	strb r3, [r4, #8]
+	LDR R6, =VALU
+	
+	LDR R7, [R6]
+	mov r8, #4
+	ldr r9, [r0,r8]
+s	B	s
+Q	DCD	0xAABBCCDD
+P	DCD	0xABCDEF78
+
+	AREA MEMORY, DATA, READWRITE
+VALU	DCD	0x11223344
+R	DCD	0xAABBCCDD
+T	DCD	0x11223344
+
+RES	SPACE	10
+	END
